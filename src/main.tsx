d0ca46566398c681db9dd2d8af6dbca3131eae50
aasd24 <39914183+aasd24@users.noreply.github.com>
2025-01-06 22:46:04 +0100

    Change to hash router to fix gh pages issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Play from './components/play/Play.tsx'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router'
+import { createHashRouter, Navigate, RouterProvider } from 'react-router'
 import Root from './components/root/Root.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <Root />,
